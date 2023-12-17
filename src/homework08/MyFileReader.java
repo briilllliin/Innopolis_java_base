2ff6f8864e4dfa7531cbfe6a4b341074276bbff6
briilllliin <briillliin@gmail.com>
2023-12-17 15:39:40 +0300

    add 8th homework

--- src/homework08/MyFileReader.java
+++ src/homework08/MyFileReader.java
@@ -1,6 +1,5 @@
 package homework08;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,24 +10,6 @@ public class MyFileReader {
 
     public final String path = "C:\\Users\\briil\\IdeaProjects\\lab1\\src\\homework08\\input.txt";
     public MyFileReader() {
-    }
-
-    public String readFromFile() throws IOException {
-
-        String text = "";
-        StringBuilder stringBuilder = new StringBuilder();
-
-
-        FileInputStream fileInputStream = new FileInputStream(path);
-        int read = fileInputStream.read();
-        while (read >= 0) {
-            char currentChar = (char) read;
-            stringBuilder.append(currentChar);
-            read = fileInputStream.read();
-        }
-        text = stringBuilder.toString();
-        return text;
-
     }
 
     public List<String> readFromLine() throws IOException {
